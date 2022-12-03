f50a3653f8f5017c824faa806414f5fb1a07ee81
Ville Koskela <vkoskela@gmail.com>
2022-12-03 14:04:25 +0200

    viikko3, teht12 tehty

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  # found quite pretty way to handle password requirements: 
+  #   https://medium.com/@Timothy_Fell/how-to-set-password-requirements-in-rails-d9081926923b
+  PASSWORD_REQUIREMENTS = /\A(?=.{4,})(?=.*[a-z])(?=.*[A-Z])/x
+  validates :password, format: PASSWORD_REQUIREMENTS
 
   has_many :ratings
   has_many :beers, through: :ratings
