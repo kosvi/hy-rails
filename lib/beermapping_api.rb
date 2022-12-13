ff08b676872848daf22be54f0e2664c8ee0cd790
Ville Koskela <vkoskela@gmail.com>
2022-12-13 21:18:58 +0200

    fix rubocop

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -22,6 +22,7 @@ class BeermappingApi
   def self.key
     return nil if Rails.env.test?
     raise 'BEERMAPPING_APIKEY env variable not defined' if ENV['BEERMAPPING_APIKEY'].nil?
+
     ENV.fetch('BEERMAPPING_APIKEY')
   end
 end
