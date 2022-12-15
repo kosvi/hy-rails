f28ff9849d9162a8e94f3b8a592690cc5aab5dc3
Ville Koskela <vkoskela@gmail.com>
2022-12-15 20:45:05 +0200

    viikko5, teht7 done

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -9,6 +9,9 @@ class BeerClubsController < ApplicationController
 
   # GET /beer_clubs/1 or /beer_clubs/1.json
   def show
+    @membership = Membership.new
+    @membership.beer_club = @beer_club
+    @membership.user = current_user
   end
 
   # GET /beer_clubs/new
