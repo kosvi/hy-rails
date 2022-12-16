628a8348512c44ef740a0e8f9b1c560f964df120
Ville Koskela <vkoskela@gmail.com>
2022-12-16 20:25:47 +0200

    lisatty style

---
+++ db/migrate/20221216182519_create_styles.rb
@@ -0,0 +1,10 @@
+class CreateStyles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :styles do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
