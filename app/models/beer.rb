f25b6a6c86a018af9a5853caf1e5f705e2cbfe57
Ville Koskela <vkoskela@gmail.com>
2022-12-16 21:51:25 +0200

    lisaa jo viitteet

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,7 @@ class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
   has_many :raters, through: :ratings, source: :user
+  belongs_to :style
 
   validates :name, presence: true
   validates :style, presence: true
--- db/migrate/20221216185959_alter_beers_to_use_styles_table.rb
+++ db/migrate/20221216185959_alter_beers_to_use_styles_table.rb
@@ -2,10 +2,20 @@ class AlterBeersToUseStylesTable < ActiveRecord::Migration[7.0]
   def up
     rename_column :beers, :style, :old_style
     add_reference :beers, :style, index: true
+    Beer.all.each do |beer|
+      style = Style.find_by name: beer.old_style
+      beer.style_id = style.id
+      beer.save
+    end
   end
 
   def down
-    rename_column :beers, :old_style, :style
+    Beer.all.each do |beer|
+      style = beer.style
+      beer.old_style = style.name
+      beer.save
+    end
     remove_reference :beers, :style, index: true
+    rename_column :beers, :old_style, :style
   end
 end
