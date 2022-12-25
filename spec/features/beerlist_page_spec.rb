54b7db8a27c97d736b494802b22425c412a79843
Ville Koskela <vkoskela@gmail.com>
2022-12-25 21:16:00 +0200

    viikko7, aloita teht4

---
+++ spec/features/beerlist_page_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Beerlist page" do
+  before :all do
+    Capybara.register_driver :chrome do |app|
+      Capybara::Selenium::Driver.new app, browser: :chrome,
+        options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+    end
+
+    Capybara.javascript_driver = :chrome
+    WebMock.allow_net_connect!
+  end
+
+  before :each do
+    @brewery1 = FactoryBot.create(:brewery, name: "Koff")
+    @brewery2 = FactoryBot.create(:brewery, name: "Schlenkerla")
+    @brewery3 = FactoryBot.create(:brewery, name: "Ayinger")
+    @style1 = Style.create name: "Lager"
+    @style2 = Style.create name: "Rauchbier"
+    @style3 = Style.create name: "Weizen"
+    @beer1 = FactoryBot.create(:beer, name: "Nikolai", brewery: @brewery1, style:@style1)
+    @beer2 = FactoryBot.create(:beer, name: "Fastenbier", brewery:@brewery2, style:@style2)
+    @beer3 = FactoryBot.create(:beer, name: "Lechte Weisse", brewery:@brewery3, style:@style3)
+  end
+
+  it "shows one known beer", js:true do
+    visit beerlist_path
+    #find('table').find('tr:nth-child(2)')
+    expect(page).to have_content "Nikolai"
+  end
+end
