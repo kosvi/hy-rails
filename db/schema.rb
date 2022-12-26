5a4aa18c9ca077e1521b4ef40ffaef2cf28aeb0d
Ville Koskela <vkoskela@gmail.com>
2022-12-26 22:08:53 +0200

    valicommit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_26_184445) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_26_200538) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_184445) do
     t.string "password_digest"
     t.boolean "admin"
     t.boolean "closed"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
