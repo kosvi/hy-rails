24e805b84c705e8f863dbe638c85f73ed176d0ba
Ville Koskela <vkoskela@gmail.com>
2022-12-27 20:40:21 +0200

    viikko7, teht9 done

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    count = ratings.count
+    count = ratings.size
     return 0 unless count > 0
 
     ratings.map(&:score).inject { |sum, num| sum + num } / count
