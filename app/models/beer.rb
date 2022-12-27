d12c57e75066d9833d1d0de83dad602163626279
Ville Koskela <vkoskela@gmail.com>
2022-12-27 22:01:49 +0200

    viikko7, teht11 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,7 @@
 class Beer < ApplicationRecord
   include RatingAverage
 
-  belongs_to :brewery
+  belongs_to :brewery, touch: true
   has_many :ratings, dependent: :destroy
   has_many :raters, through: :ratings, source: :user
   belongs_to :style
